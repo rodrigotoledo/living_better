6582592112e677070a0561d5194aaafe37a34197
rodrigo toledo <rodrigo@rtoledo.inf.br>
2022-07-09 12:02:21 -0300

    feature/dropbox_to_files - trying to save the files in dropbox active storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,8 @@
     cns: Faker::Code.isbn,
     birthday_at: "1990-01-01".to_date,
     phone: Rails.application.credentials.config[:sms_phone_number],
-    status: 1
+    status: 1,
+    avoid_sms: true
   )
   user.photo.attach(io: File.open("#{Rails.root}/spec/files/photo.png"), filename: "photo.png", content_type: "image/png")
   user.build_address(
